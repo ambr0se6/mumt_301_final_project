b9e1bd1f70619ddac3824569642e65737a837c89
Ryan Burgett <thesonicdoctor@gmail.com>
2018-12-09 17:23:08 -0500

    added favicon

--- mumt_301_final_project/app.js
+++ mumt_301_final_project/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(favicon(path.join(__dirname,'public/images', 'favicon.ico')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');

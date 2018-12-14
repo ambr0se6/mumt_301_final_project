a5dd81c2dc238cd51bf691dcc0fb6033311af653
Ryan Burgett <thesonicdoctor@gmail.com>
2018-12-14 18:35:28 -0500

    updated geoPlaylist
    - added Maps Javascript API

--- spotifytools/routes/geoPlaylist/geoPlaylist.js
+++ spotifytools/routes/geoPlaylist/geoPlaylist.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET geoPlaylist page. */
 router.get('/', function(req, res, next) {
-  res.render('geoPlaylist', { title: 'geoPlaylist' });
+  res.render('geoPlaylist', { project: 'geoPlaylist Spotify Project', reason: 'MUMT 301'});
 });
 
 module.exports = router;
--- spotifytools/routes/geoPlaylist/map.js
+++ spotifytools/routes/geoPlaylist/map.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('map', { title: 'Map' });
+  res.render('map', { step: '1' });
 });
+
 module.exports = router;
